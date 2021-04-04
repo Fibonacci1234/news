4e774983cce98e06bf8ef37f24de7ee6ecc5a31e
Fibonacci1234 <chuthikimxuan1971@gmail.com>
2021-04-04 13:50:29 +0700

    A

---
+++ technews-website-master/script.js
@@ -0,0 +1,72 @@
+
+let url = `https://api.currentsapi.services/v1/latest-news?language=en&apiKey=biLnYM8L5VoUa4Fx7L6fv52KpSDehthMXrBShWhI2AslZMPC`
+let topic = document.getElementsByClassName('banner-sub-content')[0]
+let section = document.getElementsByClassName('main-container-right')[0]
+fetch (url)
+    .then(respon => {
+        return respon.json()
+    })
+    .then((data)=>{
+        console.log(data)
+        for(let i = 0; i < data.news.length; i++){
+            let articl = data.news[i]
+            if (i < 4){
+                let hotdiv = document.createElement('div')
+                hotdiv.classList.add('hot-topic')
+                let img = document.createElement('img')
+                if(articl.image === 'None'){
+                    img.src = 'images/banner-news-1.jpg'
+                }else {           
+                    img.src = articl.image
+                }
+                let div = document.createElement('div')
+                div.classList.add('hot-topic-content')
+                let h2 = document.createElement('h2')
+                h2.innerText = articl.title
+                let p = document.createElement('p')
+                p.innerText = articl.description
+                let a = document.createElement('a')
+                a.href = articl.url
+                a.innerText = 'Read More'
+                topic.appendChild(hotdiv)
+                hotdiv.appendChild(img)
+                hotdiv.appendChild(div)
+                div.appendChild(h2)
+                div.appendChild(p)
+                div.appendChild(a)
+            }else if (i < 9){
+                let article = document.createElement('article')
+                let img = document.createElement('img')
+                if(articl.image === 'None'){
+                    img.src = 'images/banner-news-1.jpg'
+                }else {           
+                    img.src = articl.image
+                }
+                let div = document.createElement('div')
+                let h2 = document.createElement('h2')
+                h2.innerText = articl.title
+                let p = document.createElement('p')
+                p.innerText = articl.description
+                let a = document.createElement('a')
+                a.href = articl.url
+                a.innerText = 'Read More'
+                let span = document.createElement('span')
+                span.innerText = '<<<'
+                section.appendChild(article)
+                article.appendChild(img)
+                article.appendChild(div)
+                div.appendChild(h2)
+                div.appendChild(p)
+                div.appendChild(a)
+                a.appendChild(span)
+
+            }else {
+                
+            }
+        }
+    })
+    .catch(err => console.log(err))
+
+
+
+
---
+++ technews-website-master/weather-api.js
@@ -0,0 +1,35 @@
+const name = document.querySelector("#name")
+const description = document.querySelector("#description")
+const temp = document.querySelector("#temp")
+const humidity = document.querySelector("#humidity")
+const img = document.querySelector("#img")
+const wind = document.querySelector("#wind")
+const pressure = document.querySelector("#pressure")
+const date = document.querySelector("#date")
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {enableHighAccuracy: true})
+ 
+
+function successLocation(position) { 
+    console.log(position)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=58eb9b73c6ff15206d68a765952d5d49&units=metric&lang=vi`)
+    .then(response => {
+        if(response.ok) {
+            return response.json()
+        }
+    }
+)
+    .then((data => {
+        // console.log(data)
+        name.innerText = 'Thời tiết ' + data.name
+        img.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+        description.innerText =   data.weather[0].description
+        temp.innerText =  + data.main.temp + " ℃"
+        date.innerText = new Date().toLocaleString('vi-VN')
+        humidity.innerText = '💧 '+ data.main.humidity + ' %'
+        wind.innerText = "💨" + data.wind.speed + " m/s"
+        pressure.innerText = "⏲" + data.main.pressure + " Pa"
+
+    }))
+}
+function errorLocation() {}
